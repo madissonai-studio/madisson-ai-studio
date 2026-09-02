0ea05a04dc3fc9953bea387458b76deec02ba985
madissonai-studio <boringfilmcompany@gmail.com>
2026-09-03 03:27:33 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import Marketplace from "@/components/Marketplace";
+import Pricing from "@/components/Pricing";
+import AcademyTeaser from "@/components/AcademyTeaser";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Marketplace />
+      <Pricing />
+      <AcademyTeaser />
+    </>
+  );
+}
